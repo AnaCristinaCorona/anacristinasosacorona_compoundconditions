0c3daa7508a1f63b465869122126a488af90f512
AnaCristinaCorona <70342405+AnaCristinaCorona@users.noreply.github.com>
2020-10-01 14:02:49 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+let Temperature = 0
+let Farenheit = 0
+basic.forever(function () {
+    Temperature = input.temperature()
+    Farenheit = Temperature * 1.8 + 32
+    basic.showNumber(Farenheit)
+    if (Farenheit >= -22 && Farenheit <= 14) {
+        basic.showString(" Stay at home, there is heavy snow.")
+        basic.showIcon(IconNames.No)
+    } else if (Farenheit >= 15 && Farenheit <= 23) {
+        basic.showString("Go for your skis and have fun!")
+        basic.showIcon(IconNames.Yes)
+    } else if (Farenheit >= 24 && Farenheit <= 32) {
+        basic.showString("Warning! You can only ski in the highest parts.")
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (Farenheit >= 33) {
+        basic.showString("Sorry, it is not ski season.")
+        basic.showIcon(IconNames.Sad)
+    }
+})
